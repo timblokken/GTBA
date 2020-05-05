670490768832b7572918bcf7527b21f7ebcb692d
Tim Blokken <tim_blokken@hotmail.com>
2020-05-05 23:36:12 +0200

    added FranchisesviewModel

---
+++ GTBA/GTBA/ViewModels/FranchisesViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTBA.ViewModels
+{
+    public class FranchisesViewModel : BaseViewModel
+    {
+
+        public FranchisesViewModel()
+        {
+            Title = "GTBA";
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+    }
+}
