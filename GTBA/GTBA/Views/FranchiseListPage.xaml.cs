27f8129af4e8ab05e0daff07bc48cd4f5710c956
Tim Blokken <tim_blokken@hotmail.com>
2020-05-05 23:35:58 +0200

    added franchiseListPage

---
+++ GTBA/GTBA/Views/FranchiseListPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using GTBA.ViewModels;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GTBA.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FranchiseListPage : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+        FranchisesViewModel viewModel;
+
+        public FranchiseListPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new FranchisesViewModel();
+
+            Items = new ObservableCollection<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Itemssss 5"
+            };
+
+            MyListView.ItemsSource = Items;
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
