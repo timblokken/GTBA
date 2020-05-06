d642a3999ed9dd7e82d5375bf18b338c22880b57
Tim Blokken <tim_blokken@hotmail.com>
2020-05-07 00:34:34 +0200

    added movielistpage

---
+++ GTBA/GTBA/ViewModels/MoviesViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace GTBA.ViewModels
+{
+    public class MoviesViewModel : BaseViewModel
+    {
+        public ObservableCollection<string> Items { get; set; }
+        public MoviesViewModel()
+        {
+            Items = new ObservableCollection<string>
+            {
+                "movie 1",
+                "movie 2",
+                "movie 3",
+                "movie 4",
+                "movie 5"
+            };
+        }
+    }
+}
