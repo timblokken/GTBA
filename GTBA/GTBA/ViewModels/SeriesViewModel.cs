86ff96664ceab2508ad6634a81a3420e060acace
Tim Blokken <tim_blokken@hotmail.com>
2020-05-10 23:14:00 +0200

    added SeriesListPage

---
+++ GTBA/GTBA/ViewModels/SeriesViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace GTBA.ViewModels
+{
+    public class SeriesViewModel : BaseViewModel
+    {
+        public ObservableCollection<string> Items { get; set; }
+        public SeriesViewModel()
+        {
+            Items = new ObservableCollection<string>
+            {
+                "Serie 1",
+                "Serie 2",
+                "Serie 3",
+                "Serie 4",
+                "Serie 5"
+            };
+        }
+    }
+}
