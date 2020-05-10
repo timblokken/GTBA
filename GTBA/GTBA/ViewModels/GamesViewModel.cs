3aed9c8f9b29b88c1a928904ece8682d7cedf050
Tim Blokken <tim_blokken@hotmail.com>
2020-05-10 23:25:12 +0200

    added GamesListPage

---
+++ GTBA/GTBA/ViewModels/GamesViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace GTBA.ViewModels
+{
+    public class GamesViewModel : BaseViewModel
+    {
+        public ObservableCollection<string> Items { get; set; }
+        public GamesViewModel()
+        {
+            Items = new ObservableCollection<string>
+            {
+                "game 1",
+                "game 2",
+                "game 3",
+                "game 4",
+                "game 5"
+            };
+        }
+    }
+}
