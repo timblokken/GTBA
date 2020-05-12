6abedba15c5f4f7128fac51dd72a97b77e68d1fe
Tim Blokken <tim_blokken@hotmail.com>
2020-05-12 16:15:16 +0200

    created  basic models

---
+++ GTBA/GTBA/Models/Serie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTBA.Models
+{
+    public class Serie
+    {
+        public int SerieId { get; set; }
+        public string SerieName { get; set; }
+
+        public int FranchiseId { get; set; }
+        public Franchise Franchise { get; set; }
+    }
+}
