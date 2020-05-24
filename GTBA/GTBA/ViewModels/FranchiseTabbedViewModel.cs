8bd9d6fa9356421ee528305d38081d0b19165eaa
Tim Blokken <tim_blokken@hotmail.com>
2020-05-25 00:41:02 +0200

    added franchise specific tabbed page

---
+++ GTBA/GTBA/ViewModels/FranchiseTabbedViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GTBA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTBA.ViewModels
+{
+    public class FranchiseTabbedViewModel : BaseViewModel
+    {
+        MoviesViewModel moviesViewModel;
+        SeriesViewModel seriesViewModel;
+        GamesViewModel gamesViewModel;
+
+        public FranchiseTabbedViewModel(Franchise franchise)
+        {
+            Title = franchise.FranchiseName;
+
+            //moviesViewModel = new MoviesViewModel(franchise);
+            //seriesViewModel = new SeriesViewModel(franchise);
+            //gamesViewModel = new GamesViewModel(franchise);
+
+            moviesViewModel = new MoviesViewModel();
+            seriesViewModel = new SeriesViewModel();
+            gamesViewModel = new GamesViewModel();
+        }
+    }
+}
---
+++ GTBA/GTBA/Views/FranchiseTabbedPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using GTBA.Models;
+using GTBA.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GTBA.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FranchiseTabbedPage : TabbedPage
+    {
+        FranchiseTabbedViewModel viewModel;
+        public FranchiseTabbedPage(FranchiseTabbedViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
